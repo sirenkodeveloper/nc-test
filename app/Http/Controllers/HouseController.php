ab4fd69bd19b521678173c4d2ab7aa3d3c16250f
Dmitrii Sirenko <dmitriy.sirenko@gmail.com>
2022-07-25 12:58:58 +0300

    house searching route and controller

---
+++ app/Http/Controllers/HouseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\HouseSearchingRequestDTO;
+use App\Services\HouseSearchingService;
+use Illuminate\Http\Request;
+
+class HouseController {
+
+    public function index(Request $request, HouseSearchingService $houseSearchingService)
+    {
+        $houseSearchingRequestDTO = (new HouseSearchingRequestDTO())->fromRequest($request);
+        $result = $houseSearchingService->search($houseSearchingRequestDTO);
+        return response()->json($result);
+    }
+
+}
