a34ae0455ef5f1cf59e84918ef2d1c5cc47bcbe2
Dmitrii Sirenko <dmitriy.sirenko@gmail.com>
2022-07-25 18:41:23 +0300

    post

--- tests/Feature/HouseSearchingTest.php
+++ tests/Feature/HouseSearchingTest.php
@@ -22,7 +22,7 @@ class HouseSearchingTest extends TestCase
      */
     public function test_house()
     {
-        $response = $this->get('api/house');
+        $response = $this->post('api/house');
         $response->assertStatus(200);
     }
 
@@ -31,7 +31,7 @@ class HouseSearchingTest extends TestCase
         $this->seed(HouseSeeder::class);
 
         // TODO:: need to add MockHttp
-        $response = $this->get('api/house?name=The Victoria&price=374662:374662&bedrooms=4&bathrooms=2&storeys=2&garages=2');
+        $response = $this->post('api/house?name=The Victoria&price=374662:374662&bedrooms=4&bathrooms=2&storeys=2&garages=2');
         $response->assertExactJson([[
             'name' => 'The Victoria',
             'price' => 374662,
