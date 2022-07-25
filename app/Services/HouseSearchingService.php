24e2e617abb23503d9853c76df95306ed610b0ef
Dmitrii Sirenko <dmitriy.sirenko@gmail.com>
2022-07-25 12:59:21 +0300

    house searching service

---
+++ app/Services/HouseSearchingService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\HouseSearchingRequestDTO;
+
+class HouseSearchingService {
+
+    public function search(HouseSearchingRequestDTO $searchingRequestDto) {
+        return ["name" => "The Wrong"];
+    }
+
+}
