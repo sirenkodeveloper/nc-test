9d95b9ca0a08ba7f2e2933f2cfd9b597a6d3a46b
Dmitrii Sirenko <dmitriy.sirenko@gmail.com>
2022-07-25 13:02:50 +0300

    api folder

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\HouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HouseController;
 
 /*
 |--------------------------------------------------------------------------
